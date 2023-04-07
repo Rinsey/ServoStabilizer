1a97d62606e383b6dd363fdda199aa25d885bac6
ChristopherM <89361839+Rinsey@users.noreply.github.com>
2023-04-07 16:08:53 -0700

    Update setAnglesExample.ino

--- setAnglesExample/setAnglesExample.ino
+++ setAnglesExample/setAnglesExample.ino
@@ -1,62 +1,34 @@
-#define AileronPin 29
-#define ElevatorPin 28
-#define ThrottlePin 27
-#define RudderPin 26
 
 #include <ServoStabilizer.h>
 #include <Servo.h>
-#include <CrsfSerial.h>
+#include <MPU6050.h>
 
-ServoStabilizer estabilizacion(ElevatorPin,AileronPin,RudderPin,ThrottlePin);
-float roll, pitch, yaw; //Para mostrar la salida de los valores PID
-
-SerialPIO Receiver(10,11);
-CrsfSerial crsf(Receiver, 200000);|
+ServoStabilizer estabilizacion();
+float rollPID, pitchPID; //Para mostrar la salida de los valores PID
+MPU6050 mpu;
 
 void setup() {
 
   Serial.begin(115200);
-  
+  mpu.initialize();
   estabilizacion.initialize(); //La posicion de Servos a 1500 y throttle 1000 microSeconds
   estabilizacion.setGains(20.0,2.0,0.03); // valores kP, kI, kD
-  estabilizacion.setAngles(0,0,0); //Para setear ángulos a los cuales se estabilizará
-  /*
-   * La función setAngles(float,float,float); es la función a utilizar para lockear la
-   * estabilización a un angulo definido, si queremos que el avion se mantenga en un rumbo
-   * fijo pues utilizaremos la funcion para setear el angulo que queramos. Con un if es 
-   * suficiente para guardar la última posicion que el ELRS reciba al activar el switch
-   * 
-   * Ejemplo
-   * 
-   * if (crsf.getChannel(switch) > 1500) {
-   *  
-   *  estabilizacion.setAngles(angleX, angleY, angleZ); //Guardas los últimos datos de los canales
-   *  estabilizacion.update(); //Actualizaciones y salidas de PID
-   *  
-   * }else {
-   * 
-   *  angleX = crsf.getChannel(1);
-   *  angleY = crsf.getChannel(2);
-   *  angleZ = crsf.getChannel(4);
-   *  
-   * }
-   * 
-   */
+  estabilizacion.setAngles(0,0); //Para setear ángulos a los cuales se estabilizará
 }
 
 void loop() {
-
-  crsf.loop();
-  estabilizacion.update(); // Ejecutas los métodos de estabilización
   
-  pitch = estabilizacion.outputsPitch(); //.outputsPitch(); da el valor de salida PID
-  roll = estabilizacion.outputsRoll(); //.outputsRoll(); da el valor de salida PID
-  yaw = estabilizacion.outputsYaw(); //.outputsYaw(); da el valor de salida PID
-  Serial.print(roll);
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  
+  pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 180.0 / M_PI;
+  roll = atan2(ay, sqrt(ax * ax + az * az)) * 180.0 / M_PI;
+  
+  estabilizacion.update(pitch, roll); // Ejecutas los métodos de estabilización
+  pitchPID = estabilizacion.outputsPitch(); //.outputsPitch(); da el valor de salida PID en microSeconds (Correccion)
+  rollPID = estabilizacion.outputsRoll(); //.outputsRoll(); da el valor de salida PID en microSeconds (Correccion)
+  
+  Serial.print(rollPID);
   Serial.print("     ");
-  Serial.print(pitch);
-  Serial.print("     ");
-  Serial.println(yaw); 
-
+  Serial.println(pitchPID);
 }
 
