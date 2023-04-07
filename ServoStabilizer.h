8a09373d0035b6cf29036102964333e3f734dfb1
ChristopherM <89361839+Rinsey@users.noreply.github.com>
2023-04-07 16:09:58 -0700

    Update ServoStabilizer.h

--- ServoStabilizer.h
+++ ServoStabilizer.h
@@ -1,19 +1,16 @@
-
 /*	
 	Librería PID elaborada por Christopher Moyado
 	Elaborada para la estabilización de superficies de control de un aeronave
-	utilizando un MPU6050
+
 	
 	PID lib made by Christopher Moyado
 	For the stabilization of ailerons, rudder and elevator of an aircraft
-	Using a MPU6050 ELECTRIC CATS
 */
 
 #ifndef ServoStabilizer_h 
 #define ServoStabilizer_h
 
 #include <Wire.h> 
-#include <MPU6050.h> //Using electric cats lib from arduino lib manager
 #include <Arduino.h> //Not needed
 
 class ServoStabilizer {
@@ -21,7 +18,7 @@ class ServoStabilizer {
   public:
     ServoStabilizer(); //Start your object in IDE
     void initialize(); //Start the i2c communication, Servo to zero position (1500microSeconds)
-    void update(); //PID outputs here and MPU 6050 readings
+    void update(float pitchData, float rollData); //PID outputs here
     void setGains(float kp, float ki, float kd); //PID factors declarations
     void setAngles(float pitchAngle, float rollAngle); //Setpoints
 	float outputsPitch();
@@ -32,8 +29,6 @@ class ServoStabilizer {
 	float pitch, roll;
 	
   private:
-    MPU6050 mpu;
-	int16_t ax, ay, az, gx, gy, gz;
     float kp, ki, kd;
     float pitchSetpoint, rollSetpoint;
     float pitchError, rollError;
@@ -43,6 +38,5 @@ class ServoStabilizer {
     unsigned long lastTime;
 	
 };
-
 
 #endif
