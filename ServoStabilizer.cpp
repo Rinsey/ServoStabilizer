b86f91f2839b39876f713de1b10d491f9444499e
ChristopherM <89361839+Rinsey@users.noreply.github.com>
2023-04-07 16:09:45 -0700

    Update ServoStabilizer.cpp

--- ServoStabilizer.cpp
+++ ServoStabilizer.cpp
@@ -1,47 +1,31 @@
-
 /*	
 	Librería PID elaborada por Christopher Moyado
 	Elaborada para la estabilización de superficies de control de un aeronave
-	utilizando un MPU6050
 	
 	PID lib made by Christopher Moyado
 	For the stabilization of ailerons, rudder and elevator of an aircraft
-	Using a MPU6050 ELECTRIC CATS
 */
 
 #include <ServoStabilizer.h>
 
-								//int pitchPin, int rollPin, int yawPin, int throttlePin
 ServoStabilizer::ServoStabilizer() {
-  /*
-  pitchServo.attach(pitchPin);
-  rollServo.attach(rollPin);
-  /*
-  yawServo.attach(yawPin);
-  throttleServo.attach(throttlePin);
-  */
+
 }
 
 void ServoStabilizer::initialize() {
   Wire.begin();
-  mpu.initialize();
-  /* Keep at comments if you dont want to set the servos here
-  pitchServo.writeMicroseconds(1500);
-  rollServo.writeMicroseconds(1500); //SERVOS TO 0 
-  yawServo.writeMicroseconds(1500);
-  throttleServo.writeMicroseconds(1000); //Motor to 0% throttle
-  */
   delay(1000); //Not needed or can be reduced to 500ms
 }
 
-void ServoStabilizer::update() {
+void ServoStabilizer::update(float pitchData, float rollData) {
 	
-  // Get MPU6050 data
-  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  // New pitch n roll data.
+  pitch = pitchData;
+  roll = rollData;
   
-  // Calculate pitch, roll, and yaw angles
-  pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 180.0 / M_PI;
-  roll = atan2(ay, sqrt(ax * ax + az * az)) * 180.0 / M_PI;
+  //Use these equations if you want to calculate the pitch n roll
+  //pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 180.0 / M_PI;
+  //roll = atan2(ay, sqrt(ax * ax + az * az)) * 180.0 / M_PI;
   
   // Calculate errors
   pitchError = pitchSetpoint - pitch;
@@ -63,13 +47,6 @@ void ServoStabilizer::update() {
   if (pitchOutput < -500){pitchOutput = -500;}
   if (rollOutput > 500){rollOutput = 500;}
   if (rollOutput < -500){rollOutput = -500;}
-  
-  // Write outputs to servos
-  /* Keep this as comment if you dont want to write the angle directly on the servos from the library
-  pitchServo.writeMicroseconds(1500 - pitchOutput);
-  rollServo.writeMicroseconds(1500 + rollOutput);
-  yawServo.writeMicroseconds(1500 + yawOutput);
-  */
 
   // Store current values as last values
   lastTime = millis();
@@ -98,13 +75,3 @@ float ServoStabilizer::outputsPitch() {
 float ServoStabilizer::outputsRoll() {
   return rollOutput;
 }
-
-//MPU functions for outputs
-float ServoStabilizer::pitchValue(){
-	return pitch;
-}
-
-float ServoStabilizer::rollValue(){
-	return roll;
-}
- 
